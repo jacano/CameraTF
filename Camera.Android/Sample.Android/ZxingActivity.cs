328593540cd2f0a9d4324d298753f629c0e471aa
Juan Antonio Cano <jacano@plainconcepts.com>
2019-06-17 13:51:27 +0200

    Remove startup activity

--- Camera.Android/Sample.Android/ZxingActivity.cs
+++ Camera.Android/Sample.Android/ZxingActivity.cs
@@ -1,4 +1,3 @@
-using System;
 using Android.App;
 using Android.Content.PM;
 using Android.Views;
@@ -10,46 +9,13 @@ using ZXing.Net.Mobile.Android;
 
 namespace ZXing.Mobile
 {
-    [Activity (Label = "Scanner", ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.KeyboardHidden | ConfigChanges.ScreenLayout)]
+    [Activity (Label = "Scanner", MainLauncher = true, Theme = "@android:style/Theme.Holo.Light", ConfigurationChanges = ConfigChanges.Orientation | ConfigChanges.KeyboardHidden | ConfigChanges.ScreenLayout)]
     public class ZxingActivity : FragmentActivity
     {
         public static readonly string[] RequiredPermissions = new[] {
             Android.Manifest.Permission.Camera,
             Android.Manifest.Permission.Flashlight
         };
-
-        public static Action CanceledHandler;
-
-        public static Action CancelRequestedHandler;
-        public static Action<bool> TorchRequestedHandler;
-        public static Action AutoFocusRequestedHandler;
-        public static Action PauseAnalysisHandler;
-        public static Action ResumeAnalysisHandler;
-
-        public static void RequestCancel ()
-        {
-            CancelRequestedHandler?.Invoke();
-        }
-
-        public static void RequestTorch (bool torchOn)
-        {
-            TorchRequestedHandler?.Invoke(torchOn);
-        }
-
-        public static void RequestAutoFocus ()
-        {
-            AutoFocusRequestedHandler?.Invoke();
-        }
-
-        public static void RequestPauseAnalysis ()
-        {
-            PauseAnalysisHandler?.Invoke();
-        }
-
-        public static void RequestResumeAnalysis ()
-        {
-            ResumeAnalysisHandler?.Invoke();
-        }
 
         ZXingScannerFragment scannerFragment;
 
@@ -74,12 +40,6 @@ namespace ZXing.Mobile
             SupportFragmentManager.BeginTransaction ()
 				.Replace (Resource.Id.contentFrame, scannerFragment, "ZXINGFRAGMENT")
 				.Commit ();
-            
-            CancelRequestedHandler = CancelScan;
-            AutoFocusRequestedHandler = AutoFocus;
-            TorchRequestedHandler = SetTorch;
-            PauseAnalysisHandler = scannerFragment.PauseAnalysis;
-            ResumeAnalysisHandler = scannerFragment.ResumeAnalysis;
         }
 
         protected override void OnResume ()
@@ -117,7 +77,6 @@ namespace ZXing.Mobile
         public void CancelScan ()
         {
             Finish ();
-            CanceledHandler?.Invoke();
         }
 
         public override bool OnKeyDown (Keycode keyCode, KeyEvent e)
